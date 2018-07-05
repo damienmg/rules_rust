5e98aa222fab669d120af5a5c95e2dd5d0bf2f60
Damien Martin-Guillerez <dmarting@google.com>
2018-07-05 16:02:44 +0200

    Add aliases and remove support for multiple lockfiles
    Several lockfiles was not actually working and is problematic anyway
    because dependencie resolution has to happen in cargo.

--- cargo/cargo_lock_to_bzl.bzl
+++ cargo/cargo_lock_to_bzl.bzl
@@ -14,8 +14,16 @@ py_library(
             strip_prefix = "pytoml-0.1.18"
         )
 
-def _import_cargo_lockfiles_impl(rctx):
-    rctx.file("BUILD.bazel")
+def _import_cargo_lockfile_impl(rctx):
+    rctx.file(
+        "BUILD.bazel",
+        """package(default_visibility = ["//visibility:public"])
+
+load(':def.bzl', 'cargo_lockfile_aliases')
+
+cargo_lockfile_crates_aliases()
+""",
+    )
     args = [
         str(rctx.path(rctx.attr._tool)),
         "--output=def.bzl",
@@ -31,35 +39,35 @@ def _import_cargo_lockfiles_impl(rctx):
         "--additional_dep=%s=%s" % (k, v)
         for k, vl in rctx.attr.additional_deps.items()
         for v in vl
-    ] + [rctx.path(l) for l in rctx.attr.lockfiles]
+    ] + [rctx.path(rctx.attr.lockfile)]
     result = rctx.execute(
         args,
         environment={"PYTHONPATH": ":".join([rctx.path(l).dirname for l in rctx.attr._tool_deps])})
     if result.stderr:
         print(result.stderr)
 
-_import_cargo_lockfiles = repository_rule(
-    _import_cargo_lockfiles_impl,
+_import_cargo_lockfile = repository_rule(
+    _import_cargo_lockfile_impl,
     attrs = {
-        "lockfiles": attr.label_list(mandatory = True),
+        "lockfile": attr.label(mandatory = True),
         "additional_deps": attr.string_list_dict(),
         "additional_flags": attr.string_list_dict(),
         "skipped_deps": attr.string_list_dict(),
         "_tool": attr.label(default = "//rust:cargo_lock_to_bzl.py"),
-        "_tool_deps": attr.label_list(default = ["@__import_cargo_lockfiles_dep__com_github_avakar_pytoml//:BUILD.bazel"]),
+        "_tool_deps": attr.label_list(default = ["@__import_cargo_lockfile_dep__com_github_avakar_pytoml//:BUILD.bazel"]),
     },
 )
 
-def import_cargo_lockfiles(lockfiles, name="import_cargo_lockfiles", additional_deps={}, additional_flags={}, skipped_deps={}):
-    """Import dependencies declared in one or multiple cargo lockfiles
+def import_cargo_lockfile(lockfile, name="import_cargo_lockfile", additional_deps={}, additional_flags={}, skipped_deps={}):
+    """Import dependencies declared in one cargo lockfile
 
     To use, simply generate the lockfile with `cargo generate-lockfile` then refer to it from the WORKSPACE file:
     ```
     # Import the lockfile
-    load("@io_bazel_rules_rust//cargo:cargo_lock_to_bzl.bzl", "import_cargo_lockfiles")
-    import_cargo_lockfiles(["//:Cargo.lock"])
+    load("@io_bazel_rules_rust//cargo:cargo_lock_to_bzl.bzl", "import_cargo_lockfile")
+    import_cargo_lockfile(["//:Cargo.lock"])
     # Fetch the depdencies included in the lockfile
-    load("@import_cargo_lockfiles//:def.bzl", "cargo_lockfile_crates")
+    load("@import_cargo_lockfile//:def.bzl", "cargo_lockfile_crates")
     cargo_lockfile_crates()
     ```
 
@@ -67,17 +75,21 @@ def import_cargo_lockfiles(lockfiles, name="import_cargo_lockfiles", additional_
     the python binary to generate the bzl file inside the repository: `bazel run //rust:cargo_lock_to_bzl -- Cargo.lock`.
     See `bazel run //rust:cargo_lock_to_bzl -- --help` for the list of options.
 
-    Paramters:
-        name: name of the repository, default 'import_cargo_lockfiles'.
-        lockfiles: list of lockfiles to import.
+    All direct dependencies of the crate represented by the cargo lockfile will be exposed with an aliases under
+    `@import_cargo_lockfiles//:__<name>__`. For convenience a macro `cargo_lockfile_aliases` is also declared in
+    `@import_cargo_lockfiles//:def.bzl` to declare thoses aliases in your own packages.
+
+    Parameters:
+        name: name of the repository, default 'import_cargo_lockfile'.
+        lockfile: lockfile to import.
         additional_deps: dictionary from crate (<name>-<version>) to labels of dependencies to add to the given crate.
         additional_flags: dictionary from crate (<name>-<version>) to flag to pass to the given crate.
         skipped_deps: dictionary from crate (<name>-<version>) to crate dependencies (<name>-<version>) to skip on the given crate.
     """
-    _cargo_lock_deps("__import_cargo_lockfiles_dep__com_github_avakar_pytoml")
-    _import_cargo_lockfiles(
+    cargo_lock_deps("__import_cargo_lockfile_dep__com_github_avakar_pytoml")
+    _import_cargo_lockfile(
         name=name,
-        lockfiles=lockfiles,
+        lockfile=lockfile,
         additional_deps=additional_deps,
         additional_flags=additional_flags,
         skipped_deps=skipped_deps,
--- cargo/cargo_lock_to_bzl.py
+++ cargo/cargo_lock_to_bzl.py
@@ -5,88 +5,130 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "lock_file", nargs="*", help="List of lock file to parse, use '-' for stdin")
+    "input",
+    type=argparse.FileType("r"),
+    default=sys.stdin,
+    help="Lock file to parse")
 parser.add_argument(
     "--name",
-    default="cargo_lockfile_crates",
+    default="cargo_lockfile",
     help="Name of the macro to generate")
 parser.add_argument(
     "--additional_dep",
     action="append",
     default=[],
-    help="New dependency to add to crate <name> in the form <name>-<version>=//label"
+    help=
+    "New dependency to add to crate <name> in the form <name>-<version>=//label"
 )
 parser.add_argument(
     "--additional_flag",
     action="append",
     default=[],
-    help="New flag to add to crate <name> in the form <name>-<version>=--flag"
-)
+    help="New flag to add to crate <name> in the form <name>-<version>=--flag")
 parser.add_argument(
     "--skip_dep",
     action="append",
     default=[],
-    help="Dependency to remove from crate <name> in the form <name>-<version>=<depname>-<depversion>")
+    help=
+    "Dependency to remove from crate <name> in the form <name>-<version>=<depname>-<depversion>"
+)
 parser.add_argument(
     "--output",
     type=argparse.FileType("w"),
     default=sys.stdout,
     help="output file")
 
-def cargo_crate(**kwargs):
-    kwargs = {k:v for k,v in kwargs.items() if v != None}
-    return "cargo_crate(%s)" % (", ".join(["%s=%s" % (k, repr(v)) for k, v in kwargs.items()]))
 
-def lock_to_bzl(fin, fout, name, ctxt):
+def cargo_crate(**kwargs):
+    kwargs = {k: v for k, v in kwargs.items() if v != None}
+    return "cargo_crate(%s)" % (
+        ", ".join(["%s=%s" % (k, repr(v)) for k, v in kwargs.items()]))
+
+
+def alias(**kwargs):
+    kwargs = {k: v for k, v in kwargs.items() if v != None}
+    return "native.alias(%s)" % (
+        ", ".join(["%s=%s" % (k, repr(v)) for k, v in kwargs.items()]))
+
+
+def lock_to_bzl(fin, fout, fn_name, ctxt):
     lock_file = pytoml.load(fin)
+    do_pass = True
     fout.write("""
 load("@io_bazel_rules_rust//cargo:crate_repository.bzl", "cargo_crate")
 
-def %s():""" % name)
+def %s_crates():\n""" % fn_name)
     for p in lock_file["package"]:
+        # TODO(damienmg): support other sources that crates.io-index.
         if "source" in p and p["source"].endswith("crates.io-index"):
             name = p["name"].encode("UTF-8")
             version = p["version"].encode("UTF-8")
             versioned_name = "%s-%s" % (name, version)
-            skipped_deps = set(ctxt["skpped_deps"][versioned_name] if versioned_name in ctxt["skipped_deps"] else [])
+            skipped_deps = set(ctxt["skipped_deps"][versioned_name] if
+                               versioned_name in ctxt["skipped_deps"] else [])
             source = p["source"].encode("UTF-8")
-            locked_deps = [d.encode("UTF-8").split(" ", 2) for d in p["dependencies"]] if "dependencies" in p else []
-            locked_deps = [d for d in locked_deps if not ("%s-%s" % (d[0], d[1])) in skipped_deps]
+            locked_deps = [
+                d.encode("UTF-8").split(" ", 2) for d in p["dependencies"]
+            ] if "dependencies" in p else []
+            locked_deps = [
+                d for d in locked_deps
+                if not ("%s-%s" % (d[0], d[1])) in skipped_deps
+            ]
             checksum_key = "checksum %s %s (%s)" % (name, version, source)
-            checksum = lock_file["metadata"][checksum_key].encode("UTF-8") if checksum_key in lock_file["metadata"] else None
-            flags = ctxt["added_flags"][versioned_name] if versioned_name in ctxt["added_flags"] else None
-            added_deps = ctxt["added_deps"][versioned_name] if versioned_name in ctxt["added_deps"] else None
-            fout.write("\n  " + cargo_crate(
+            checksum = lock_file["metadata"][checksum_key].encode(
+                "UTF-8") if checksum_key in lock_file["metadata"] else None
+            flags = ctxt["added_flags"][
+                versioned_name] if versioned_name in ctxt[
+                    "added_flags"] else None
+            added_deps = ctxt["added_deps"][
+                versioned_name] if versioned_name in ctxt[
+                    "added_deps"] else None
+            do_pass = False
+            fout.write("  ")
+            fout.write(cargo_crate(
                 name=name,
                 version=version,
-                locked_deps={d[0]: d[1] for d in locked_deps},
+                locked_deps={d[0]: d[1]
+                             for d in locked_deps},
                 additional_deps=added_deps,
                 flags=flags,
                 sha256=checksum))
-    fout.write("\n")
+            fout.write("\n")
+    if do_pass:
+        fout.write("  pass\n")
+    fout.write("""
+def %s_aliases():\n""" % fn_name)
+    do_pass = True
+    for p in lock_file["package"]:
+        if "source" not in p:
+            for d in p["dependencies"]:
+                do_pass = False
+                d = d.encode("UTF-8").split(" ", 2)
+                fout.write("  ")
+                fout.write(
+                    alias(
+                        name=d[0],
+                        actual="@io_crates_{name}__{version}//:{name}".format(
+                            name=d[0].replace("-", "_"), version=d[1].replace(".", "_"))))
+                fout.write("\n")
+    if do_pass:
+        fout.write("  pass\n")
 
 def dep_context(args):
     return {
-        "added_deps": {
-            v.split("=", 1)[0]: v.split("=", 1)[1]
-            for v in args.additional_dep
-        },
-        "added_flags": {
-            v.split("=", 1)[0]: v.split("=", 1)[1]
-            for v in args.additional_flag
-        },
-        "skipped_deps": {
-            v.split("=", 1)[0]: v.split("=", 1)[1]
-            for v in args.skip_dep
-        }
+        "added_deps":
+        {v.split("=", 1)[0]: v.split("=", 1)[1]
+         for v in args.additional_dep},
+        "added_flags":
+        {v.split("=", 1)[0]: v.split("=", 1)[1]
+         for v in args.additional_flag},
+        "skipped_deps":
+        {v.split("=", 1)[0]: v.split("=", 1)[1]
+         for v in args.skip_dep}
     }
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     ctxt = dep_context(args)
-    for f in args.lock_file:
-        if f == "-":
-            lock_to_bzl(sys.stdin, args.output, args.name, ctxt)
-        else:
-            with open(f, "r") as fin:
-                lock_to_bzl(fin, args.output, args.name, ctxt)
+    lock_to_bzl(args.input, args.output, args.name, ctxt)
